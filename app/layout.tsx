e4952e6e6d079c572a66de27132d1537cd8b4988
Toukir Ahamed Pigeon <toukir.ahamed.pigeon@gmail.com>
2025-03-15 14:32:16 +0600

    All

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,7 +16,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <ClerkProvider signInFallbackRedirectUrl="/">
+    <ClerkProvider signInFallbackRedirectUrl="/" publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY}>
       <html lang="en">
         <body
           className={`${GeistSans.className} ${GeistMono.variable} antialiased`}
